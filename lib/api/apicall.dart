d3f3eb5358afcaf14e8ed90d7ce6b408465a1ca7
AjithS <ajiths@agilexcyber.in>
2022-08-02 12:17:31 +0530

    api call completed

--- lib/api/apicall.dart
+++ lib/api/apicall.dart
@@ -1,4 +1,32 @@
-// ApiCall
-// {
+import 'dart:convert';
 
-// }
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiCall {
+  final int id;
+  final String title;
+  final String body;
+  const ApiCall({required this.id, required this.title, required this.body});
+  factory ApiCall.formJson(Map<String, dynamic> json) {
+    return ApiCall(
+        id: json['id'] as int,
+        title: json['title'] as String,
+        body: json['body'] as String);
+  }
+}
+
+Future<List<ApiCall>> getdata() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+  var dummy = compute(listformation, response.body);
+  print('the response is:$dummy');
+  return dummy;
+}
+
+List<ApiCall> listformation(String responsenbody) {
+  final convertdata = jsonDecode(responsenbody);
+  print(convertdata);
+  return convertdata.map<ApiCall>((json) => ApiCall.formJson(json)).toList();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:listemail/api/apicall.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,22 +19,71 @@ class MyListView extends StatefulWidget {
 }
 
 class _MyListViewState extends State<MyListView> {
-  final List<String> entries = <String>['A', 'B', 'C', 'D'];
-  final List<int> colorCodes = <int>[700, 600, 500, 200];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Api List")),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: entries.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 50,
-              color: Colors.amber[colorCodes[index]],
-              child: Center(child: Text('Entry ${entries[index]}')),
-            );
-          }),
+        appBar: AppBar(title: const Text("Api List")),
+        body: FutureBuilder<List<ApiCall>>(
+          future: getdata(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Mylistview(data: snapshot.data!);
+            } else if (snapshot.hasError) {
+              return Text("Error has occured${snapshot.error}");
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ));
+  }
+}
+
+class Mylistview extends StatelessWidget {
+  const Mylistview({super.key, required this.data});
+  final List<ApiCall> data;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        return Container(
+            margin: EdgeInsets.only(bottom: 10),
+            // width: MediaQuery.of(context).size.width * 0.2,
+            child: Row(
+              children: [
+                CircleAvatar(
+                  child: Text(
+                    data[index].title[0].toUpperCase(),
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                  backgroundColor: Colors.lightGreenAccent,
+                ),
+                Text("  "),
+                Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        data[index].title.toUpperCase(),
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.80,
+                      child: Text(
+                        data[index].body,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    // Text(data[index].body),
+                  ],
+                )
+
+                //Text(data.toString()),
+              ],
+            ));
+      },
     );
   }
 }
